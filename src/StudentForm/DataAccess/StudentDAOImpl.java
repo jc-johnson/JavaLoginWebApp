6dab0ecbf940fd7f07de3e7232d8d41e11491b4e
jc-johnson <jc-johnson@users.noreply.github.com>
2017-12-16 18:35:07 -0800

    add student sql statement

--- src/StudentForm/DataAccess/StudentDAOImpl.java
+++ src/StudentForm/DataAccess/StudentDAOImpl.java
@@ -2,12 +2,43 @@ package StudentForm.DataAccess;
 
 import StudentForm.beans.StudentBean;
 
+import java.sql.*;
 import java.util.List;
 
 public class StudentDAOImpl implements StudentDAO {
 
+    private Connection initialize() {
+        // JDBC driver name and database URL
+        final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+        final String DB_URL = "jdbc:mysql://localhost/test";
+
+        //  Database credentials
+        final String USER = "username";
+        final String PASS = "password";
+
+
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            //STEP 2: Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+
     @Override
-    public void saveOrUpdate(StudentBean student) {
+    public void saveOrUpdate(StudentBean studentBean) {
 
     }
 
@@ -23,6 +54,22 @@ public class StudentDAOImpl implements StudentDAO {
 
     @Override
     public StudentBean get(int studentId) {
+
+        Connection connection = initialize();
+
+        if (connection != null) {
+            System.out.println("Creating statement...");
+
+            try {
+                Statement statement = connection.createStatement();
+                String sqlQuery;
+                sqlQuery = "SELECT id FROM Students";
+                ResultSet resultSet = statement.executeQuery(sqlQuery);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
         return null;
     }
 
---
+++ src/StudentForm/resources/InsertStudents.sql
@@ -0,0 +1,18 @@
+INSERT INTO student VALUES (110, 'Bryce', 
+	'Myers', '12343 lovers lane', '7788888888', 'Neil Armstrong', 'M');
+INSERT INTO student VALUES (null, 'Layla', 
+	'Ali', '343 dixie road', '7783333388', 'Woodgate', 'F');
+INSERT INTO student VALUES (null, 'Doug', 
+	'Werner', '343423 rocky road', '7734992388', 'Woodgate', 'M');
+INSERT INTO student VALUES (null, 'Stephanie', 
+	'Schumer', '343 dixie road', '7783333388', 'Woodgate', 'F');
+INSERT INTO student VALUES (null, 'Mark', 
+	'Clock', '556 roomy street', '3113333388', 'Neil Armstrong', 'M');
+INSERT INTO student VALUES (null, 'Pamla', 
+	'Marsh', '111 jackson way', '8889992222', 'Coleman', 'F');
+INSERT INTO student VALUES (null, 'Jason', 
+	'Parker', '3333 winster street', '7783333388', 'Coleman', 'M');
+INSERT INTO student VALUES (null, 'Brock', 
+	'Jackson', '32 dixie road', '5592223434', 'Woodgate', 'M');
+INSERT INTO student VALUES (null, 'Jane', 
+	'Doe', '12 sunny road', '3838 E washington street', 'Coleman', 'F');
