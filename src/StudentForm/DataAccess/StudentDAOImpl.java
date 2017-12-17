838e530d31cd71c6ad303148099c960b73d777e2
jc-johnson <jc-johnson@users.noreply.github.com>
2017-12-16 21:59:08 -0800

    update to student sql statement

--- src/StudentForm/DataAccess/StudentDAOImpl.java
+++ src/StudentForm/DataAccess/StudentDAOImpl.java
@@ -10,18 +10,19 @@ public class StudentDAOImpl implements StudentDAO {
     private Connection initialize() {
         // JDBC driver name and database URL
         final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-        final String DB_URL = "jdbc:mysql://localhost/test";
+        final String DB_URL = "jdbc:mysql://localhost:3306/test";
+        // jdbc:mysql://127.0.0.1:3306/?user=root;
 
         //  Database credentials
-        final String USER = "username";
-        final String PASS = "password";
+        final String USER = "root";
+        final String PASS = "root";
 
 
         Connection connection = null;
         Statement statement = null;
         try {
             //STEP 2: Register JDBC driver
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName(JDBC_DRIVER);
 
             //STEP 3: Open a connection
             System.out.println("Connecting to database...");
@@ -56,25 +57,80 @@ public class StudentDAOImpl implements StudentDAO {
     public StudentBean get(int studentId) {
 
         Connection connection = initialize();
+        Statement statement = null;
+        StudentBean studentBean = null;
 
         if (connection != null) {
             System.out.println("Creating statement...");
 
             try {
-                Statement statement = connection.createStatement();
+                statement = connection.createStatement();
                 String sqlQuery;
-                sqlQuery = "SELECT id FROM Students";
+                sqlQuery = "SELECT idStudent FROM Student";
                 ResultSet resultSet = statement.executeQuery(sqlQuery);
+
+                //Extract data from result set
+                while(resultSet.next()){
+                    if (resultSet.getInt("idStudent") == studentId) {
+
+                        int id  = resultSet.getInt("idStudent");
+                        String first = resultSet.getString("firstName");
+                        String last = resultSet.getString("lastName");
+                        String address = resultSet.getString("address");
+                        String phone = resultSet.getString("phoneNumber");
+                        String school = resultSet.getString("school");
+                        String sex = resultSet.getString("sex");
+
+                        studentBean.setFirstName(first);
+                        studentBean.setLastName(last);
+                        studentBean.setAddress(address);
+                        studentBean.setPhoneNumber(phone);
+                        studentBean.setSchoolName(school);
+                        studentBean.setSex(sex);
+
+                        // Display values
+                        System.out.print("ID: " + id);
+                        System.out.print(", First: " + first);
+                        System.out.println(", Last: " + last);
+                        System.out.println(", Address: " + address);
+                        System.out.println(", Phone: " + phone);
+                        System.out.println(", School: " + school);
+                        System.out.println(", Sex: " + sex);
+                    }
+                }
+                // Clean-up environment
+                resultSet.close();
+                statement.close();
+                connection.close();
             } catch (SQLException e) {
                 e.printStackTrace();
+            }finally{
+                // finally block used to close resources
+                try{
+                    if(statement!=null)
+                        statement.close();
+                }catch(SQLException sqlException){
+                    sqlException.printStackTrace();
+                }
+                try{
+                    if(connection!=null)
+                        connection.close();
+                }catch(SQLException se){
+                    se.printStackTrace();
+                }
             }
         }
 
-        return null;
+        return studentBean;
     }
 
     @Override
     public List<StudentBean> list() {
         return null;
+    }
+
+    public static void main(String[] args) {
+        StudentDAO studentDAO = new StudentDAOImpl();
+        StudentBean studentBean = studentDAO.get(113);
     }
 }
---
+++ src/Tests/TestStudentForm.java
@@ -0,0 +1,13 @@
+package Tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestStudentForm {
+
+
+    @Test
+    public void TestSqlAddStatement() {
+
+    }
+}
