e87569308100baf7e8112a1831b9e3776c9b8de6
jc-johnson <jc-johnson@users.noreply.github.com>
2017-12-17 01:02:39 -0800

    added test to add student to db

--- src/Tests/TestStudentForm.java
+++ src/Tests/TestStudentForm.java
@@ -1,13 +1,17 @@
 package Tests;
 
+import StudentForm.DataAccess.StudentDAO;
+import StudentForm.DataAccess.StudentDAOImpl;
+import StudentForm.beans.StudentBean;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TestStudentForm {
 
-
     @Test
     public void TestSqlAddStatement() {
-
+        StudentDAO studentDAO = new StudentDAOImpl();
+        StudentBean studentBean = studentDAO.get(113);
+        Assert.assertEquals("Stephanie", studentBean.getFirstName());
     }
 }
